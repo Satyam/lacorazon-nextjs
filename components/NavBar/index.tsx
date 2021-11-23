d807fc82d6a631f5a854b6f89c18782e953c053d
Satyam <satyam@satyam.com.ar>
2021-11-23 18:36:42 +0100

    Dropped the manual collapse of the NavBar since react-bootstrap takes care of that. Added a Container component to NavBar to keep it away from the borders

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -5,6 +5,7 @@ import classNames from 'classnames';
 import {
   Navbar,
   Nav,
+  Container,
   // UncontrolledDropdown,
   // DropdownToggle,
   // DropdownMenu,
@@ -15,47 +16,43 @@ import { FaUser } from 'react-icons/fa';
 import styles from './styles.module.css';
 
 export default function NavBar() {
-  const [isOpen, setOpen] = useState(false);
   // const { isAuthenticated, loginWithPopup, logout, user } = useAuth0();
   // const { locale, setLocale, locales } = useIntl();
-  function toggle() {
-    setOpen(!isOpen);
-  }
-
   return (
     <div>
       <Navbar expand="md" bg="light" className={styles.navbar}>
-        <Link href="/">
-          <a className={classNames('navbar-brand', styles.navbrand)}>
-            <Image
-              src="/La Corazon.png"
-              alt="La Corazón"
-              layout="fixed"
-              width="20"
-              height="20"
-            />{' '}
-            La Corazón
-          </a>
-        </Link>
-        <Navbar.Toggle onClick={toggle} />
-        <Navbar.Collapse>
-          <Nav className="ml-auto" navbar>
-            <Nav.Item>
-              <Link href="/users" passHref>
-                <Nav.Link>Usuarios</Nav.Link>
-              </Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Link href="/distribuidores" passHref>
-                <Nav.Link>Distribuidores</Nav.Link>
-              </Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Link href="/ventas" passHref>
-                <Nav.Link>Ventas</Nav.Link>
-              </Link>
-            </Nav.Item>
-            {/* <UncontrolledDropdown nav inNavbar>
+        <Container>
+          <Link href="/">
+            <a className={classNames('navbar-brand', styles.navbrand)}>
+              <Image
+                src="/La Corazon.png"
+                alt="La Corazón"
+                layout="fixed"
+                width="20"
+                height="20"
+              />{' '}
+              La Corazón
+            </a>
+          </Link>
+          <Navbar.Toggle />
+          <Navbar.Collapse>
+            <Nav className="ml-auto" navbar>
+              <Nav.Item>
+                <Link href="/users" passHref>
+                  <Nav.Link>Usuarios</Nav.Link>
+                </Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Link href="/distribuidores" passHref>
+                  <Nav.Link>Distribuidores</Nav.Link>
+                </Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Link href="/ventas" passHref>
+                  <Nav.Link>Ventas</Nav.Link>
+                </Link>
+              </Nav.Item>
+              {/* <UncontrolledDropdown nav inNavbar>
               <DropdownToggle nav caret>
                 {locale}
               </DropdownToggle>
@@ -100,8 +97,9 @@ export default function NavBar() {
                 </>
               )}
             </UncontrolledDropdown> */}
-          </Nav>
-        </Navbar.Collapse>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
     </div>
   );
