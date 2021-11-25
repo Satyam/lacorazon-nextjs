8772183e2f55dd74cb5ffe85f30bfca1d7376a13
Satyam <satyam@satyam.com.ar>
2021-11-25 18:10:34 +0100

    expanded /api/users to  handle inserts and updates

--- pages/api/users/[id].ts
+++ pages/api/users/[id].ts
@@ -1,26 +1,38 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import type { User } from 'data/types';
 
-import { del, get } from 'data/user';
+import { del, get, update, create } from 'data/user';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<User | undefined>
 ) {
   const { id } = req.query;
-  let reply: User | undefined;
+
+  const standardReply = (reply: User | undefined) => {
+    if (reply) {
+      res.json(reply);
+    } else {
+      res.status(404).end('not found');
+    }
+  };
 
   switch (req.method) {
     case 'DELETE':
-      reply = await del(id as ID);
+      standardReply(await del(id as ID));
       break;
     case 'GET':
-      reply = await get(id as ID);
+      standardReply(await get(id as ID));
       break;
-  }
-  if (reply) {
-    res.json(reply);
-  } else {
-    res.status(404).end('not found');
+    case 'PUT': {
+      const user = JSON.parse(req.body) as User;
+      await (id === '0' ? create(user) : update(user))
+        .then(standardReply)
+        .catch((error) => {
+          if (error.errno === 19) {
+            res.status(409).end('Duplicate user name');
+          }
+        });
+    }
   }
 }
