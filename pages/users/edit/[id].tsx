9165c4b47e39c729bdd31384d667345dbc1bbeae
Satyam <satyam@satyam.com.ar>
2021-11-25 18:12:41 +0100

    Working on user edit/add new

--- pages/users/edit/[id].tsx
+++ pages/users/edit/[id].tsx
@@ -2,20 +2,21 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import * as yup from 'yup';
 
-import { Form, TextField, SubmitButton } from 'components/Form';
+import {
+  Form,
+  TextField,
+  SubmitButton,
+  OnFormSubmitFunction,
+} from 'components/Form';
 import Layout from 'components/Layout';
 import { ButtonIconAdd, ButtonIconDelete, ButtonSet } from 'components/Icons';
 import { Loading, useModals, Alert } from 'components/Modals';
-import { useGetUser, FetchError } from 'util/fetch';
+import { useGetUser, FetchError, upsertUser, deleteUser } from '../utils';
+import type { User } from 'data/types';
 
 // import { useAuth0 } from 'Providers/Auth';
-// import { UseFormMethods } from 'react-hook-form';
 
-type UserType = {
-  id?: ID;
-  nombre?: string;
-  email?: string;
-};
+type UserType = Omit<Partial<User>, 'password'>;
 
 const userSchema = yup.object().shape({
   email: yup.string().trim().email().default(''),
@@ -25,15 +26,12 @@ const userSchema = yup.object().shape({
 export default function EditUser() {
   const router = useRouter();
   const { alert } = useModals();
-  const { id } = router.query;
+  const idUser = router.query.id as ID;
+  const id: ID | undefined = idUser === 'new' ? undefined : idUser;
 
   const { data: user, error } = useGetUser(id as ID);
 
-  // const createUser = useCreateUser();
-  // const updateUser = useUpdateUser();
-  // const deleteUser = useDeleteUser();
   const { openLoading, closeLoading, confirmDelete } = useModals();
-  // const [gqlErr, setGqlErr] = useState<string | false>(false);
   // const { can } = useAuth0();
 
   if (error) {
@@ -45,71 +43,104 @@ export default function EditUser() {
       );
     }
     return (
-      <Alert warning heading="Desconocido" onClose={() => router.back()}>
+      <Alert warning heading="Error Desconocido" onClose={() => router.back()}>
         {error.message}
       </Alert>
     );
   }
 
-  if (!id || !user) return <Loading>Cargando usuario</Loading>;
+  if (id && !user) return <Loading>Cargando usuario</Loading>;
 
   const onDeleteClick: React.MouseEventHandler<HTMLButtonElement> = (ev) => {
     ev.stopPropagation();
     const { nombre, id } = ev.currentTarget.dataset;
-    confirmDelete(
-      `al usuario ${nombre}`,
-      () => console.log('should delete', id, nombre)
-      // deleteUser(id as string)
-      //   .then(() => router.replace('/users'))
-      //   .catch((err) => {
-      //     if (err.message === 'GraphQL error: unauthorized') {
-      //       setGqlErr('No está autorizado para borrar el usuario');
-      //     } else throw err;
-      //   })
-    );
+    confirmDelete(`al usuario ${nombre}`, () => {
+      console.log('should delete', id, nombre);
+      deleteUser(id as string).then(({ data, error }) => {
+        if (error) {
+          if (error instanceof FetchError && error.status === 404) {
+            return (
+              <Alert heading="No existe" warning onClose={() => router.back()}>
+                El vendedor ya había sido borrado
+              </Alert>
+            );
+          }
+          return (
+            <Alert
+              warning
+              heading="Error Desconocido"
+              onClose={() => router.back()}
+            >
+              {error.message}
+            </Alert>
+          );
+        }
+        router.back();
+      });
+    });
   };
 
-  const onSubmit = async (
-    values: UserType,
-    // formMethods: UseFormMethods
-  ): Promise<void> => {
+  const onSubmit: OnFormSubmitFunction<UserType> = async (
+    values,
+    formReturn
+  ) => {
     if (id) {
       openLoading('Actualizando usuario');
       console.log('should update', id, values);
-      // await updateUser(id, values)
-      //   .catch((err) => {
-      //     if (
-      //       err.message ===
-      //       'GraphQL error: SQLITE_CONSTRAINT: UNIQUE constraint failed: Users.nombre'
-      //     ) {
-      //       formMethods.setError('nombre', {
-      //         type: 'duplicate',
-      //         message: 'Ese usuario ya existe',
-      //       });
-      //     } else throw err;
-      //   })
-      //   .finally(closeLoading);
+      await upsertUser({ id, ...values })
+        .then(({ data, error }) => {
+          if (error instanceof FetchError) {
+            switch (error.status) {
+              case 404:
+                return (
+                  <Alert
+                    heading="No existe"
+                    warning
+                    onClose={() => router.back()}
+                  >
+                    El vendedor ya había sido borrado
+                  </Alert>
+                );
+              case 409:
+                formReturn.setError('nombre', {
+                  type: 'duplicado',
+                  message: 'Ese nombre ya existe',
+                });
+                return (
+                  <Alert heading="Duplicado" warning onClose={() => undefined}>
+                    Ya existe un vendedor con ese mismo nombre
+                  </Alert>
+                );
+            }
+          }
+        })
+        .finally(closeLoading);
     } else {
       openLoading('Creando usuario');
       console.log('should create', values);
-      // await createUser({ ...values, password: values.nombre })
-      //   .then((id) => {
-      //     router.replace(`/user/edit/${id}`);
-      //   })
-      //   .catch((err) => {
-      //     if (
-      //       err.message ===
-      //       'GraphQL error: SQLITE_CONSTRAINT: UNIQUE constraint failed: Users.nombre'
-      //     ) {
-      //       formMethods.setError('nombre', {
-      //         type: 'duplicate',
-      //         message: 'Ese usuario ya existe',
-      //       });
-      //     } else throw err;
-      //   })
-      //   .finally(closeLoading);
+      await upsertUser({ ...values, password: values.nombre })
+        .then(({ data, error }) => {
+          console.log('after insert, upsert returned', { data, error });
+          if (error instanceof FetchError) {
+            switch (error.status) {
+              case 409:
+                formReturn.setError('nombre', {
+                  type: 'duplicado',
+                  message: 'Ese nombre ya existe',
+                });
+                return (
+                  <Alert heading="Duplicado" warning onClose={() => undefined}>
+                    Ya existe un vendedor con ese mismo nombre
+                  </Alert>
+                );
+            }
+          }
+          router.replace(`/users/edit/${id}`);
+        })
+        .finally(closeLoading);
     }
   };
+
   return (
     <Layout
       title={`Vendedor - ${user ? user.nombre : 'nuevo'}`}
@@ -128,13 +159,13 @@ export default function EditUser() {
             {id ? 'Modificar' : 'Agregar'}
           </SubmitButton>
           {/* {id && can('user:delete') && ( */}
-            <ButtonIconDelete
-              data-id={id}
-              data-nombre={user && user.nombre}
-              onClick={onDeleteClick}
-            >
-              Borrar
-            </ButtonIconDelete>
+          <ButtonIconDelete
+            data-id={id}
+            data-nombre={user && user.nombre}
+            onClick={onDeleteClick}
+          >
+            Borrar
+          </ButtonIconDelete>
           {/* )} */}
         </ButtonSet>
       </Form>
